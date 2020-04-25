5c03944d70181507ef064cfe271560f81642b3d5
Abas Hersi <55505133+abis206@users.noreply.github.com>
2020-04-25 14:36:26 -0700

    Create equals.java

---
+++ equals.java
@@ -0,0 +1,28 @@
+/**
+View Problem Here: https://practiceit.cs.washington.edu/problem/view/bjp4/chapter16/e15-equals
+*/
+
+public boolean equals2(LinkedIntList list) {
+    ListNode current1 = front;
+    ListNode current2 = list.front;
+
+    if (current1 == null && current2 == null) {
+        return true;
+    } else if (current1 != null && current2 != null) {
+
+        while (current1 != null && current2 != null) {
+
+            if (current1.data != current2.data) {
+                return false;
+            } else {
+                current1 = current1.next;
+                current2 = current2.next;
+            }
+
+        }
+
+        return current1 == null && current2 == null;
+    }
+
+    return false;
+}
